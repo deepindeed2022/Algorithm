f2be56fe21220aa779087137baad32f9c4bfbe44
cwlseu <caowenlong92@gmail.com>
2017-04-22 15:08:15 +0800

    <dev>submit 551-student-attenance-Record.cpp</dev>

---
+++ cpp/leetcode/551-student-attenance-Record.cpp
@@ -0,0 +1,19 @@
+
+class Solution {
+ public:
+   bool checkRecord(string s) {
+      int len = s.length();
+      int a_count = 0;
+      for (int i = 0; i < len; i++) {
+         if (s[i] == 'A')
+            a_count++;
+         if (a_count > 1)
+            return false;
+         if (i < len - 1 && i > 0) {
+            if (s[i] == s[i - 1] && s[i] == s[i + 1] && s[i] == 'L')
+               return false;
+         }
+      }
+      return true;
+   }
+};
