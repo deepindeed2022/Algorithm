c18a7171a2884e68c665f4f49e1256f1962b9ce2
cwlseu <caowenlong92@gmail.com>
2017-06-24 15:32:41 +0800

    <dev> 铺砖问题，通过归纳法</dev>

---
+++ jianzhioffer/rectCover.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+class Solution:
+    def rectCover(self, number):
+        # write code here
+       	if number <= 2:
+       		return number
+       	else:
+       		# return rectCover(n - 1) + rectCover(n -2)
+       		a = 1
+       		b = 2
+       		for i in xrange(number -1):
+       			a, b = b, a + b
+       		return a
+
+solve = Solution()
+for i in xrange(1, 10):
+	print "%d:%d" % (i, solve.rectCover(i))
