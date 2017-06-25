a3eb4302f14b3bbdd5b93cbc10da618322600699
cwlseu <caowenlong92@gmail.com>
2017-06-25 22:41:43 +0800

    <dev>查找最后kth nodes</dev>

---
+++ jianzhioffer/FindkthToTail.cpp
@@ -0,0 +1,29 @@
+/*
+struct ListNode {
+	int val;
+	struct ListNode *next;
+	ListNode(int x) :
+			val(x), next(NULL) {
+	}
+};*/
+class Solution {
+public:
+    ListNode* FindKthToTail(ListNode* pListHead, unsigned int k) {
+    	ListNode* fast = pListHead;
+    	int i = k;
+    	while(fast && i > 0)
+    	{
+    		fast = fast->next;
+    		i--;
+    	}
+    	if(i != 0) return NULL;
+
+    	ListNode* slow = pListHead;
+    	while(fast && slow)
+    	{
+    		fast  = fast->next;
+    		slow = slow->next;
+    	}
+    	return slow;
+    }
+};
