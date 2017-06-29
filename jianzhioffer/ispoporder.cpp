13c32f70adb610a7e7ddae5683bcd1a91f779141
cwlseu <caowenlong92@gmail.com>
2017-06-30 00:37:04 +0800

    <dev>isPopOrder提交</dev>

---
+++ jianzhioffer/ispoporder.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool IsPopOrder(vector<int> pushV, vector<int> popV) {
+        if(pushV.size() != popV.size()) return false;
+        vector<int> stack;
+        for(int i = 0, j = 0; i < pushV.size();)
+        {
+        	stack.push_back(pushV[i++]);
+        	while(j < popV.size() && stack.back() == popV[j])
+        	{
+        		stack.pop_back();
+        		j++;
+        	}
+        }
+        return stack.empty();
+    }
+};
