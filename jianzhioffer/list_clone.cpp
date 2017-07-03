1c34a2c42e5383aa94a7e473c6e4b4314e159cf3
cwlseu <caowenlong92@gmail.com>
2017-07-03 23:43:51 +0800

    <dev>剑指offer</dev>

---
+++ jianzhioffer/list_clone.cpp
@@ -0,0 +1,56 @@
+
+// 链接：https://www.nowcoder.com/questionTerminal/f836b2c43afc4b35ad6adc41ec941dba
+
+
+
+struct RandomListNode {
+    int label;
+    struct RandomListNode *next, *random;
+    RandomListNode(int x) :
+            label(x), next(NULL), random(NULL) {
+    }
+};
+
+
+    /*
+        1、复制每个节点，如：复制节点A得到A1，将A1插入节点A后面
+        2、遍历链表，A1->random = A->random->next;
+        3、将链表拆分成原链表和复制后的链表
+    */
+class Solution {
+public:
+    RandomListNode* Clone(RandomListNode* pHead)
+    {
+        if(!pHead) return NULL;
+        RandomListNode* currNode = pHead;
+        while(currNode)
+        {
+        	RandomListNode* node = new  RandomListNode(currNode->label);
+        	node->next = currNode->next;
+        	currNode->next = node;
+        	// 在原list中走
+        	currNode = node->next;
+        }
+        currNode = pHead;
+        while(currNode)
+        {
+        	RandomListNode* node = currNode->next;
+        	if(currNode->random)
+        	{
+        		node->random = currNode->random->next;
+        	}
+        	currNode = node->next;
+        }
+
+
+		RandomListNode *pCloneHead = pHead->next;
+        RandomListNode *tmp;
+        currNode = pHead;
+        while(currNode->next){
+            tmp = currNode->next;
+            currNode->next =tmp->next;
+            currNode = tmp;
+        }
+        return pCloneHead;
+    }
+};
