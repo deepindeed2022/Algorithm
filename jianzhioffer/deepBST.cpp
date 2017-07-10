f442a127896f2835a8c230e9babd5e0d27fdc413
cwlseu <caowenlong92@gmail.com>
2017-07-10 23:35:50 +0800

    <dev>剑指offer</dev>

--- jianzhioffer/deepBST.cpp
+++ jianzhioffer/deepBST.cpp
@@ -1,21 +1,46 @@
-/*
+#include <iostream>
+#include <queue>
+using namespace std;
+
 struct TreeNode {
 	int val;
 	struct TreeNode *left;
 	struct TreeNode *right;
 	TreeNode(int x) :
-			val(x), left(NULL), right(NULL) {
+		val(x), left(NULL), right(NULL) {
 	}
-};*/
-class Solution {
-public:
-    // int TreeDepth(TreeNode* pRoot)
-    // {
-    // 	if(pRoot==NULL) return 0;
-    //     return 1 + max(TreeDepth(pRoot->left), TreeDepth(pRoot->right));
-    // }
-    int TreeDepth(TreeNode* pRoot)
-    {
-    	
-    }
+};
+class Solution
+{
+ public:
+   // int TreeDepth(TreeNode* pRoot)
+   // {
+   // 	if(pRoot==NULL) return 0;
+   //     return 1 + max(TreeDepth(pRoot->left), TreeDepth(pRoot->right));
+   // }
+   int TreeDepth(TreeNode *pRoot)
+   {
+      if (pRoot == NULL)
+         return 0;
+      int depth     = 0;
+
+      queue<TreeNode *> que;
+      que.push(pRoot);
+      while (!que.empty())
+      {
+         int size = que.size();
+         depth++;
+         for (int i = 0; i < size; i++)
+         {
+         	TreeNode *node = que.front();
+            que.pop();
+            if (node->left)
+               que.push(node->left);
+            if (node->right)
+               que.push(node->right);
+         }
+
+      }
+      return depth;
+   }
 };
