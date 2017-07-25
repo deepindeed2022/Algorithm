0a2b9d57e0b05db5bec938e428898ca991b4e792
cwlseu <caowenlong92@gmail.com>
2017-07-26 00:32:22 +0800

    <dev>NVIDIA面试</dev>

--- jianzhioffer/numberof1.cpp
+++ jianzhioffer/numberof1.cpp
@@ -1,4 +1,6 @@
 #include <bitset>
+#include <iostream>
+
 using namespace std;
 
 class Solution
@@ -16,8 +18,33 @@ class Solution
       }
       return count;
    }
+   int NumberOf1_2(int n)
+   {
+      // bitset<32> bit(n);
+      //  return bit.count();
+      int count = 0;
+      while (n)
+      {
+         if(n^(n-1))
+            count++;
+         n = n & (n - 1);
+      }
+      return count;
+   }
    int Numberof1(int n) { 
       bitset<32> bt(n); 
       return bt.count();
     }
 };
+ostream& bin(ostream& os, int i)
+{
+   os << bitset<32>(i);
+   return os;
+}
+int main(int argc, char const *argv[])
+{
+   Solution solve;
+   for(int i = 0xFFFFFFF0; i < 0xFFFFFFFF; i++)
+      bin(std::cout, i)<<" : " << solve.NumberOf1(i)  <<" " <<solve.NumberOf1_2(i) << std::endl;
+   return 0;
+}
