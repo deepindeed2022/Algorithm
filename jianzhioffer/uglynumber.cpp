b8afc3e30a68fcb378ec48347f5a11cd819d9b02
cwlseu <caowenlong92@gmail.com>
2017-08-02 00:00:25 +0800

    add

---
+++ jianzhioffer/uglynumber.cpp
@@ -0,0 +1,18 @@
+
+class Solution {
+public://别人的代码就是精简，惭愧啊，继续学习。
+    int GetUglyNumber_Solution(int index) {
+        if (index < 7)return index;
+        vector<int> res(index);
+        res[0] = 1;
+        int t2 = 0, t3 = 0, t5 = 0, i;
+        for (i = 1; i < index; ++i)
+        {
+            res[i] = min(res[t2] * 2, min(res[t3] * 3, res[t5] * 5));
+            if (res[i] == res[t2] * 2)t2++;
+            if (res[i] == res[t3] * 3)t3++;
+            if (res[i] == res[t5] * 5)t5++;
+        }
+        return res[index - 1];
+    }
+};
