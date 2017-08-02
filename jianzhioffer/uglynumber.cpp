e03ffea2877c258221d4389e4fca1ecce9876354
cwlseu <caowenlong92@gmail.com>
2017-08-02 23:36:40 +0800

    1

--- jianzhioffer/uglynumber.cpp
+++ jianzhioffer/uglynumber.cpp
@@ -1,18 +1,27 @@
+#include <vector>
+#include <iostream>
+using namespace std;
 
 class Solution {
-public://别人的代码就是精简，惭愧啊，继续学习。
-    int GetUglyNumber_Solution(int index) {
-        if (index < 7)return index;
-        vector<int> res(index);
-        res[0] = 1;
-        int t2 = 0, t3 = 0, t5 = 0, i;
-        for (i = 1; i < index; ++i)
-        {
-            res[i] = min(res[t2] * 2, min(res[t3] * 3, res[t5] * 5));
-            if (res[i] == res[t2] * 2)t2++;
-            if (res[i] == res[t3] * 3)t3++;
-            if (res[i] == res[t5] * 5)t5++;
-        }
-        return res[index - 1];
-    }
+public:
+	int GetUglyNumber_Solution(int index) {
+		if (index < 7) return index;
+		vector<int> res(index);
+		res[0] = 1;
+		int t2 = 0, t3 = 0, t5 = 0, i;
+		for (i = 1; i < index; ++i)
+		{
+			res[i] = min(res[t2] * 2, min(res[t3] * 3, res[t5] * 5));
+			if (res[i] == res[t2] * 2)t2++;
+			if (res[i] == res[t3] * 3)t3++;
+			if (res[i] == res[t5] * 5)t5++;
+		}
+		return res[index - 1];
+	}
 };
+int main(int argc, char const *argv[])
+{
+	Solution solve;
+	std::cout << solve.GetUglyNumber_Solution(7) << std::endl;
+	return 0;
+}
