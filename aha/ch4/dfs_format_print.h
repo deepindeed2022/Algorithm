39a507ef884e154988e28e864aa1894f55b8190e
cwlseu <caowenlong92@gmail.com>
2017-08-13 22:42:37 +0800

    <dev> update the code </dev>

--- aha/ch4/dfs_format_print.h
+++ aha/ch4/dfs_format_print.h
@@ -21,10 +21,10 @@ bool inMaze(int tx, int ty, int maze_w, int maze_h)
  * @param path[in] 输入的结果路径，其中元素为不同节点的位置坐标
  * @param w, h[in] 该路径是在一个w*h的平面上进行绘制
  **/
-void format_path(std::vector<std::pair<int, int>> &path, int w, int h,
+void format_path(std::vector<std::pair<int, int> > &path, int w, int h,
                  ostream &os = std::cout)
 {
-   std::vector<std::vector<std::string>> result(
+   std::vector<std::vector<std::string> > result(
       h, std::vector<std::string>(w, "-  "));
 #define x first
 #define y second
